465f27cb8beefd5078b2bdd459792e921a1b9a8f
Ben Hanrahan <hanrahan.ben@gmail.com>
2011-10-06 15:47:03 -0400

    added code to play a bit nicer with other extensions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## status-menu-additions
 
-Alternative to the alternative-status-menu@gnome-shell-extensions.gnome.org. That version wipes out the status menu, this one just adds a few items to it.
+Alternative to the alternative-status-menu@gnome-shell-extensions.gnome.org. That version wipes out the status menu, this one just adds a few items to it.  This was shamelessly copied from http://blog.fpmurphy.com/2011/05/more-gnome-shell-customization.html, with one addition.  There is a check for whether or not this is the suspend is the last item in the list, if it is not a separator will be inserted.  This is so that it plays a bit nicer with other extensions.
 
 ---
 
--- src/extension.js
+++ src/extension.js
@@ -1,41 +1,78 @@
-/* -*- mode: js2 - indent-tabs-mode: nil - js2-basic-offset: 4 -*- */
-const Lang = imports.lang;
 const St = imports.gi.St;
-
 const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
-const GnomeSession = imports.misc.gnomeSession;
-
-const Gettext = imports.gettext.domain('gnome-shell-extensions');
+const Shell = imports.gi.Shell;
+const Lang = imports.lang;
+const Gettext = imports.gettext.domain('gnome-shell');
 const _ = Gettext.gettext;
-
+ 
+function updateSuspend(object, pspec, item) {
+    item.actor.visible = object.get_can_suspend();
+}
+ 
 function updateHibernate(object, pspec, item) {
     item.actor.visible = object.get_can_hibernate();
 }
-
+ 
+function onSuspendActivate(item) {
+    Main.overview.hide();
+ 
+    this._screenSaverProxy.LockRemote(Lang.bind(this, function() {
+        this._upClient.suspend_sync(null);
+    }));
+}
+ 
 function onHibernateActivate(item) {
     Main.overview.hide();
-
+ 
     this._screenSaverProxy.LockRemote(Lang.bind(this, function() {
         this._upClient.hibernate_sync(null);
     }));
 }
+ 
+function changeUserMenu()
+{
+    let addSeparator = false;
+    let children = this.menu._getMenuItems();
+    for (let i = 0; i < children.length; i++) {
+        let item = children[i];
+        if (item.label) {
+            let _label = item.label.get_text();
+            // global.log("menu label: " + _label);
+            if (_label == _("Suspend")) {
+                item.destroy();
+                // check if this is the last item in the list
+                if ( i != children.length - 1 ) {
+                    addSeparator = true;
+                }
+            }
+        }
+    }
 
-function addItems() {
+    if (addSeparator) 
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+ 
+    let item = new PopupMenu.PopupMenuItem(_("Suspend"));
+    item.connect('activate', Lang.bind(this, onSuspendActivate));
+    this._upClient.connect('notify::can-suspend', Lang.bind(this, updateSuspend, item));
+    updateSuspend(this._upClient, null, item);
+    this.menu.addMenuItem(item);
+ 
     item = new PopupMenu.PopupMenuItem(_("Hibernate"));
     item.connect('activate', Lang.bind(this, onHibernateActivate));
     this._upClient.connect('notify::can-hibernate', Lang.bind(this, updateHibernate, item));
     updateHibernate(this._upClient, null, item);
     this.menu.addMenuItem(item);
-
+ 
     item = new PopupMenu.PopupMenuItem(_("Power Off..."));
     item.connect('activate', Lang.bind(this, function() {
-	this._session.ShutdownRemote();
+        this._session.ShutdownRemote();
     }));
     this.menu.addMenuItem(item);
 }
-
+ 
 function main(metadata) {
+    // Post 3.0  let statusMenu = Main.panel._userMenu;
     let statusMenu = Main.panel._statusmenu;
-    addItems.call(statusMenu);
+    changeUserMenu.call(statusMenu);
 }
